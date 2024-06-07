142a490b8c32070c3128aba7a3413d5576938a25
Simeon Gejn <simeon.gejn@schueler.ers-hm.de>
2024-06-07 19:18:55 +0200

    Added "Playagain" HUDObject.

--- App/Scoreboard.cs
+++ App/Scoreboard.cs
@@ -70,34 +70,35 @@ namespace Aimlabs.App
         public override void Prepare()
         {
             playagain = new HUDObjectImage();
-            playagain.SetPosition(720, 360);
+            playagain.SetPosition(Window.Width / 2, Window.Height / 7 );
             playagain.Name = "name";
             playagain.SetScale(300);
+            playagain.SetTexture("./App/Textures/playagain.png");
             AddHUDObject(playagain);
 
             botscorex = new HUDObjectText("Botscore:" + Stats.botscore);
-            botscorex.SetPosition(100f, 100f);
+            botscorex.SetPosition(Window.Width / 4, 100f);
             botscorex.Name = "Botscore";
             botscorex.SetScale(18);
             botscorex.SetFont(FontFace.NovaMono);
             AddHUDObject(botscorex);
 
             ballscorex = new HUDObjectText("Ballscore:" + Stats.ballscore);
-            ballscorex.SetPosition(100f, 166f);
+            ballscorex.SetPosition(Window.Width / 4, 166f);
             ballscorex.Name = "Botscore";
             ballscorex.SetScale(18);
             ballscorex.SetFont(FontFace.NovaMono);
             AddHUDObject(ballscorex);
 
             clicks = new HUDObjectText("Clicks:" + Stats.leftmouseclicks);
-            clicks.SetPosition(1000f, 166f);
+            clicks.SetPosition(Window.Width / 1.5f, 166f);
             clicks.Name = "clicks";
             clicks.SetScale(18);
             clicks.SetFont(FontFace.NovaMono);
             AddHUDObject(clicks);
 
             accuracy = new HUDObjectText("Accuracy:" + Stats.accuracy + "%");
-            accuracy.SetPosition(1000f, 100f);
+            accuracy.SetPosition(Window.Width / 1.5f, 100f);
             accuracy.Name = "accuracy";
             accuracy.SetScale(18);
             accuracy.SetFont(FontFace.NovaMono);
