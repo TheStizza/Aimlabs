94a532bcc2d5e8e977af2d56eafad5dedd3fcea0
Lutz Karau <lutz.karau@ers-hameln.de>
2024-06-08 11:48:29 +0200

    updated engine library to support borderless windowed mode

--- App/GameWindow.cs
+++ App/GameWindow.cs
@@ -4,7 +4,13 @@ namespace Aimlabs.App
 {
     public class GameWindow : GLWindow
     {
-        public GameWindow() : base(1920, 1080, true, PostProcessingQuality.High, WindowMode.BorderlessWindow)
+        public GameWindow() : base(
+            Math.Min(1920, KWEngine.ScreenInformation.PrimaryScreenWidth),                                          // Breite des primären Bildschirms
+            Math.Min(1080, KWEngine.ScreenInformation.PrimaryScreenHeight),                                         // Höhe des primären Bildschirms
+            true,                                                                                                   // VSync an?
+            PostProcessingQuality.High,                                                                             // Renderqualität
+            KWEngine.ScreenInformation.PrimaryScreenWidth > 1920 ? WindowMode.Default : WindowMode.BorderlessWindow // Art des Fensters (Standard oder rahmenlos)
+            )
         {
             Title = "Aimlabs v0.0.1";
 
